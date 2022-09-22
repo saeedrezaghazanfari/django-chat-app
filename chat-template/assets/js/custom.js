694bba5babcb0450c0728a3160636f49f826c871
سعیدرضا غضنفری <saeedreza.gh.1397@gmail.com>
2022-09-22 17:24:22 +0330

    structure of static files is ok!

--- chat-template/assets/js/custom.js
+++ chat-template/assets/js/custom.js
@@ -5,33 +5,6 @@ function rand(min , max){
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-// persian digit
-$(document).ready(function(){
-    $('.digit').persiaNumber();
-});
-
-function showPW() {
-
-    var x = document.getElementById("pwLOGIN");
-    var y = document.getElementById('eye');
-    var yy = document.getElementById('eye2');
-
-    if (x.type === "password") {
-    x.type = "text";
-    y.classList.remove('fa-eye');
-    y.classList.add('fa-eye-slash');
-    yy.classList.remove('fa-eye');
-    yy.classList.add('fa-eye-slash');
-
-    } else {
-    x.type = "password";
-    y.classList.add('fa-eye');
-    y.classList.remove('fa-eye-slash');
-    yy.classList.add('fa-eye');
-    yy.classList.remove('fa-eye-slash');
-    }
-}
-
 clockDefner = () =>{
 
     var m;
@@ -71,14 +44,3 @@ clockDefner = () =>{
 }
 // document.getElementById('clock').innerHTML = setInterval("clockDefner()" , 1000);  
 
-
-// scroll events
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//     var currentScrollPos = window.pageYOffset;
-//     if (currentScrollPos < 100) {
-//         document.getElementById("a").style.display = "none";
-//     } else {
-//         document.getElementById("a").style.display = "block";
-//     }
-// }
