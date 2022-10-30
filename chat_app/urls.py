f6823616130feec5bed27a95e733c8aae6eec127
SaeedReza Ghazanfari <saeedreza.gh.1397@gmail.com>
2022-10-31 00:09:44 +0330

    base of fetch method

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('django-chat-app/auth/', views.chatapp_auth),
     path('join-chat/<str:username>', views.join_chat),
 ]
--- chat_app/views.py
+++ chat_app/views.py
@@ -1,16 +1,20 @@
 import json
-from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 
 
 # url: / 
 def home(request):
-    return render(request, 'home.html', {
-        'title': settings.CHATAPP_TITLE,
-        'sub_title': settings.CHATAPP_SUBTITLE
-    })
+    return render(request, 'home.html')
 
+# url: /django-chat-app/auth/
+@csrf_exempt
+def chatapp_auth(request):
+    if request.method == 'POST':        
+        return JsonResponse({'data': 'saved'})
+    return JsonResponse({'data': 'Bad Request'})
 
 # url: /join-chat/<str:username>
 def join_chat(request, username):
