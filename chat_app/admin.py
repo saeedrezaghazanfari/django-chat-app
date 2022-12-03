3204426cd2a23ff6c437117a97fd87dd4bccac4f
SaeedReza Ghazanfari <saeedreza.gh.1397@gmail.com>
2022-12-03 23:11:43 +0330

    actions for admin is ok

--- chat_app/admin.py
+++ chat_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from .models import (
     SupporterModel,
     ChatModel,
@@ -7,6 +9,42 @@ from .models import (
 )
 
 
+# write actions for UserChatModel
+@admin.action(description=_('حذف کاربران غیرفعال در ۳۰ روز اخیر'))
+def del_users_more_30d(modeladmin, request, queryset):
+    onemonth_ago = timezone.now() - timezone.timedelta(days=30)
+    UserChatModel.objects.filter(
+        created__lt=onemonth_ago
+    ).delete()
+
+
+@admin.action(description=_('حذف کاربرانی که مسدود شده اند'))
+def del_users_isblocked(modeladmin, request, queryset):
+    if queryset.is_blocked:
+        queryset.delete()
+    else:
+        UserChatModel.objects.filter(
+            is_blocked=True
+        ).delete()
+
+
+@admin.action(description=_('آزادکردن کاربرانی که مسدود شده اند'))
+def unblock_users(modeladmin, request, queryset):
+    if queryset:
+        queryset.update(
+            is_blocked=False,
+            report_numbers=0
+        )
+    else:
+        UserChatModel.objects.filter(
+            is_blocked=True
+        ).update(
+            is_blocked=False,
+            report_numbers=0
+        )
+
+
+# write admin models
 class SupporterModel_Admin(admin.ModelAdmin):
     list_display = ['user', 'supporter_uid', 'full_name', 'is_active']
     search_field = ['supporter_uid', 'fullname']
@@ -23,6 +61,7 @@ class UserChatModel_Admin(admin.ModelAdmin):
     list_display = ['user_chat_uid', 'created', 'is_blocked']
     search_field = ['user_chat_uid', 'email', 'phone', 'is_blocked']
     ordering = ['-id']
+    actions = [del_users_more_30d, del_users_isblocked, unblock_users]
 
     
 class ReadyChatModel_Admin(admin.ModelAdmin):
